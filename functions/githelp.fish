f5cee8d9436a140d2344a0313dbf2173f97cd151
Pascal Roux <pascal.roux.fr@gmail.com>
2018-12-27 17:42:14 +0100

    help

--- functions/githelp.fish
+++ functions/githelp.fish
@@ -1,5 +1,8 @@
 function githelp -d "My package helping for Git"
   # Package entry-point
-  echo "githelp"
+  echo "githelp usage"
+  echo "gitback : return to the previous branch"
+  echo "gitbacklist : show the list of the previous branches"
+  echo "gitquickcommit: git add + commit + push"
 end
 
